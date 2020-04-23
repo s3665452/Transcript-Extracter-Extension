7a8232de36c64de510a710476aa4697a61d7658b
Antonwonn <42013631+Antonwonn@users.noreply.github.com>
2020-04-23 23:01:29 +1000

    Add content script

--- background.js
+++ background.js
@@ -1,14 +1,8 @@
-chrome.runtime.onInstalled.addListener(function() {
-    chrome.storage.sync.set({color: '#3aa757'}, function() {
-      console.log("video!");
-    });
-    chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
-      chrome.declarativeContent.onPageChanged.addRules([{
-        conditions: [new chrome.declarativeContent.PageStateMatcher({
-          css: ["video"],
-        })
-        ],
-            actions: [new chrome.declarativeContent.ShowPageAction()]
-      }]);
-    });
-  });
+console.log('background running');
+
+chrome.browserAction.onClicked.addListener(buttonClicked);
+
+function buttonClicked(tab){
+    chrome.tabs.sendMessage(tab.id, "hello")
+    console.log('message sent');
+}
--- popup.js
+++ popup.js
@@ -1,5 +1,7 @@
-let download = document.getElementById('download');
+let Download = document.getElementById('download');
 
-download.onclick = function() {
-download("https://d1b5wfhwav7hwn.cloudfront.net/content/3dc53221-7293-46aa-96ce-173670b449a5/20/04/17/10/3dc53221-7293-46aa-96ce-173670b449a5_1_200417T101903874Z.mp4?Expires=1587560123&Signature=gkZY4-693zvfLHszlE5EafdZyrOyMWUkjtFlvvdUl~NxXqncIcDQ9Hxp41g4RHeRB8xhMauUomMj6izo4mP4uIO559FedqO-PITkVaa6Iqceg1abfjRAlqY7yreNltxDLmaSv8Bl2Rfp2KwErTTjla0KPLodtozzssrp3RR1614pjeNUAJYzPVk~In4rHrcRtOkwfstGVn5nsRElLL908iHaGrsso4ScUtfLXSR6u4Ls62pyQqRtQldc~~bvVrXYugIOaDEUM7fkoh~B9n9tQ92s2F8iTUCCkq69wT-SVUgcT-lOoA1fLnjgtzmF1vvz5Xl9FnGuttz9WGrNQK7d4A__&Key-Pair-Id=APKAIOBDBIMXUOQOBYVA", "sf.mp4", "text/plain");
+
+
+Download.onclick = function(tab) {
+    chrome.tabs.sendMessage(tab.id, "download");
   };
